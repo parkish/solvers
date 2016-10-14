06f8ff78d2a062f4ccfc024c38c7d460d0562e02
Richard Park <parkish>
2016-10-14 13:59:10 -0600

    starting to get a dotnet core project going

---
+++ sudoku/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication
+{
+    public class Data 
+    {
+        public string Version { get; set;}
+    } 
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Data data = null;
+
+            using (var reader = File.OpenText("project.json")) 
+            {
+                using (var jsonTextReader = new JsonTextReader(reader)) 
+                {
+                    var jsonSerializer = new JsonSerializer();
+                    data = jsonSerializer.Deserialize(jsonTextReader, typeof(Data)) as Data; 
+                }
+            }
+            
+            Console.WriteLine("Loaded version = {0}", data.Version);
+        }
+    }
+}
